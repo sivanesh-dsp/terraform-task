039a8522333361eebe6dab2d55bcb479dbcbc941
Sivanesh B <60127773+sivanesh-dsp@users.noreply.github.com>
2024-08-26 11:04:31 +0530

    Update install-docker.sh (sonarqube)

--- jenkins-setup-module/install-docker.sh
+++ jenkins-setup-module/install-docker.sh
@@ -29,3 +29,16 @@ sudo usermod -aG docker $USER
 
 # Print Docker version to confirm installation
 sudo docker --version
+
+# docker volumes for sonarqube to be persist
+sudo docker volume create sonarqube_data
+sudo docker volume create sonarqube_logs
+sudo docker volume create sonarqube_extensions
+
+# installing sonarqube
+sudo docker run -d --name sonarqube \
+    -p 9000:9000 \
+    -v sonarqube_data:/opt/sonarqube/data \
+    -v sonarqube_logs:/opt/sonarqube/logs \
+    -v sonarqube_extensions:/opt/sonarqube/extensions \
+    sonarqube:lts-community
